8d91460767d8adc0c3b358459f91689bf7c6a86d
bryanRT <toncelbryan17@gmail.com>
2024-08-25 22:31:07 -0500

    segunda parte de la pagina de netfix version mia

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const fila = document.querySelector(".contenedor-carousel");
+const pelicula = document.querySelectorAll(".pelicula");
+
+const flechaIzquierda = document.getElementById("flecha-izquierda");
+const flechaDerecha = document.getElementById("flecha-derecha");
+
+// ? ----- ----- Event Listener para la flecha derecha. ----- -----
+flechaDerecha.addEventListener("click", () => {
+  fila.scrollLeft += fila.offsetWidth;
+
+  const indicadorActivo = document.querySelector("indicadores.activo");
+  if (indicadorActivo.nextSibling) {
+    indicadorActivo.nextSibling.classList.add("activo");
+  }
+});
+
+// ? ----- ----- Event Listener para la flecha izquierda. ----- -----
+flechaIzquierda.addEventListener("click", () => {
+  fila.scrollLeft -= fila.offsetWidth;
+
+  const indicadorActivo = document.querySelector(".indicadores .activo");
+  if (indicadorActivo.previousSibling) {
+    indicadorActivo.previousSibling.classList.add("activo");
+    indicadorActivo.classList.remove("activo");
+    indicadorActivo.classList.remove("activo");
+  }
+});
+
+// ? ----- ----- Paginacion ----- -----
+const numeroPaginas = Math.ceil(pelicula.length / 5);
+for (let i = 0; i < numeroPaginas; i++) {
+  const indicador = document.createElement("button");
+
+  if (i === 0) {
+    indicador.classList.add("activo");
+  }
+
+  document.querySelector(".indicadores").appendChild(indicador);
+  indicador.addEventListener("click", (e) => {
+    fila.scrollLeft = i * fila.offsetWidth;
+
+    document.querySelector(".indicadores .activo").classList.remove("activo");
+    e.target.classList.add("activo");
+  });
+}
